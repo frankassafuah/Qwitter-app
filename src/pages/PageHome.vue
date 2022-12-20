e05a75803f900f9a1ccd68ad108469195bbf3dcc
Frank Assafuah <frankassafuah@gmail.com>
2022-12-20 21:30:07 +0000

    Final touches

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,76 +1,85 @@
 <template>
-  <q-page>
-    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
-      <div class="col">
-        <q-input
-          class="new-qweet"
-          bottom-slots
-          v-model="newQweetContent"
-          placeholder="What's happening?"
-          counter
-          maxlength="280"
-          autogrow
+  <q-page class="relative-position">
+    <q-scroll-area class="absolute fullscreen">
+      <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+        <div class="col">
+          <q-input
+            class="new-qweet"
+            bottom-slots
+            v-model="newQweetContent"
+            placeholder="What's happening?"
+            counter
+            maxlength="280"
+            autogrow
+          >
+            <template v-slot:before>
+              <q-avatar size="xl">
+                <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+              </q-avatar>
+            </template>
+          </q-input>
+        </div>
+        <div class="col col-shrink">
+          <q-btn
+            @click="addNewQweet"
+            class="q-mb-lg"
+            unelevated
+            rounded
+            color="primary"
+            label="Qweet"
+            no-caps
+            :disable="!newQweetContent"
+          />
+        </div>
+      </div>
+      <q-separator class="divider" size="10px" color="grey-2" />
+      <!-- tweet list -->
+      <q-list separator>
+        <transition-group
+          appear
+          enter-active-class="animated fadeIn"
+          leave-active-class="animated fadeOut"
         >
-          <template v-slot:before>
-            <q-avatar size="xl">
-              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
-            </q-avatar>
-          </template>
-        </q-input>
-      </div>
-      <div class="col col-shrink">
-        <q-btn
-          @click="addNewQweet"
-          class="q-mb-lg"
-          unelevated
-          rounded
-          color="primary"
-          label="Qweet"
-          no-caps
-          :disable="!newQweetContent"
-        />
-      </div>
-    </div>
-    <q-separator class="divider" size="10px" color="grey-2" />
-    <!-- tweet list -->
-    <q-list separator>
-      <q-item v-for="qweet in mapQweets" :key="qweet.id">
-        <q-item-section avatar top>
-          <q-avatar size="xl">
-            <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
-          </q-avatar>
-        </q-item-section>
+          <q-item v-for="qweet in mapQweets" :key="qweet.id" class="q-py-md">
+            <q-item-section avatar top>
+              <q-avatar size="xl">
+                <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+              </q-avatar>
+            </q-item-section>
 
-        <q-item-section>
-          <q-item-label class="text-subtitle1">
-            <strong> Frank Assafuah </strong>
-            <span class="text-grey-7">@frank_assafuah</span>
-          </q-item-label>
-          <q-item-label class="qweet-content text-body1">
-            {{ qweet.content }}
-          </q-item-label>
-          <div class="qweet-icons row justify-between q-mt-sm">
-            <q-btn color="grey" size="sm" icon="far fa-comment" flat round>
-            </q-btn>
-            <q-btn color="grey" size="sm" icon="fas fa-retweet" flat round>
-            </q-btn>
-            <q-btn color="grey" size="sm" icon="far fa-heart" flat round>
-            </q-btn>
-            <q-btn
-              color="grey"
-              size="sm"
-              icon="fas fa-trash"
-              flat
-              round
-              @click="deleteQweet(qweet.id)"
-            >
-            </q-btn>
-          </div>
-        </q-item-section>
-
-        <q-item-section side top> {{ qweet.date }} </q-item-section>
-      </q-item>
-    </q-list>
+            <q-item-section>
+              <q-item-label class="text-subtitle1">
+                <strong> Frank Assafuah </strong>
+                <span class="text-grey-7"
+                  >@frank_assafuah <br class="lt-md" />
+                  &bull; {{ qweet.date }}</span
+                >
+              </q-item-label>
+              <q-item-label class="qweet-content text-body1">
+                {{ qweet.content }}
+              </q-item-label>
+              <div class="qweet-icons row justify-between q-mt-sm">
+                <q-btn color="grey" size="sm" icon="far fa-comment" flat round>
+                </q-btn>
+                <q-btn color="grey" size="sm" icon="fas fa-retweet" flat round>
+                </q-btn>
+                <q-btn color="grey" size="sm" icon="far fa-heart" flat round>
+                </q-btn>
+                <q-btn
+                  color="grey"
+                  size="sm"
+                  icon="fas fa-trash"
+                  flat
+                  round
+                  @click="deleteQweet(qweet.id)"
+                >
+                </q-btn>
+              </div>
+            </q-item-section>
+          </q-item>
+        </transition-group>
+      </q-list>
+    </q-scroll-area>
   </q-page>
 </template>
 
